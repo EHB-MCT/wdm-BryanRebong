2aeba3ed682fdf7b0461f9230cc5b4d37a1da8a9
BryanRebong <bryan.rebong@student.ehb.be>
2025-10-16 15:14:01 +0200

    imported vue files of webpages into the index.js file

---
+++ karaoke-frontend/src/router/index.js
@@ -0,0 +1,5 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "../pages/Home.vue";
+import Genres from "../pages/Genres.vue";
+import Songs from "../pages/Songs.vue";
+
