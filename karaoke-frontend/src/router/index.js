ebbf5ed69552a8f737583664f928cc37e0ab0175
BryanRebong <bryan.rebong@student.ehb.be>
2025-10-16 15:20:13 +0200

    added basic route code for the webpages on the frontend

--- karaoke-frontend/src/router/index.js
+++ karaoke-frontend/src/router/index.js
@@ -3,3 +3,15 @@ import Home from "../pages/Home.vue";
 import Genres from "../pages/Genres.vue";
 import Songs from "../pages/Songs.vue";
 
+const routes = [
+    { path: "/", component: home},
+    { path: "/genres", component: genre},
+    { path: "/songs/:genre", component: Songs},
+];
+
+const router = createRouter ({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
