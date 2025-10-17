f7b30fd1363f04452e12a6414c1efdf5d60b926d
BryanRebong <bryan.rebong@student.ehb.be>
2025-10-17 09:36:51 +0200

    create new js file to keep the data of all the songs in the project

---
+++ karaoke-frontend/src/data/songs.js
@@ -0,0 +1,7 @@
+export const songs = {
+    pop: [
+        { title: "Love Yourself", artist: "Justin Bieber" },
+        { title: "Die For You", artist: "The Weeknd" },
+        { title: "Just The Way U Are", artist: "Bruno Mars" }
+    ]
+};
