de1ea07d6406a57afbe1bb3654539c66d68f519c
BryanRebong <bryan.rebong@student.ehb.be>
2025-10-17 09:46:21 +0200

    added more genres and songs onto the songs.js file

--- karaoke-frontend/src/data/songs.js
+++ karaoke-frontend/src/data/songs.js
@@ -3,5 +3,20 @@ export const songs = {
         { title: "Love Yourself", artist: "Justin Bieber" },
         { title: "Die For You", artist: "The Weeknd" },
         { title: "Just The Way U Are", artist: "Bruno Mars" }
-    ]
+    ],
+    rock: [
+        { title: "Don't Stop Believin'", artist: "Journey" },
+        { title: "Thunderstruck", artist: "ACDC" },
+        { title: "Life Is a Highway", artist: "Rascall Flatts" }
+    ],
+    hiphop: [
+        { title: "Beat It", artist: "Sean Kingston" },
+        { title: "Wall To Wall", artist: "Chris Brown" },
+        { title: "Folded", artist: "Kehlani" }
+    ],
+    kpop: [
+        { title: "Fashion", artist: "CORTIS" },
+        { title: "Crazy", artist: "LESSERAFIM" },
+        { title: "Jellyous", artist: "ILLIT" }
+    ]    
 };
