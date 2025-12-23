5587c135c3dc204bd349b707889f2c45e307ea12
BryanRebong <bryan.rebong@student.ehb.be>
2025-12-23 15:35:54 +0100

    imported code from useMicrophone.js onto home.vue to test function, added a button for the function

--- karaoke-frontend/src/pages/Home.vue
+++ karaoke-frontend/src/pages/Home.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="home">
-    <h1>Welcome to KaraoKey!</h1>
-    <router-link to="/genres">
-      <button>Start Singing</button>
-    </router-link>
-  </div>
+    <div class="home">
+        <h1>Welcome to KaraoKey!</h1>
+        <button @click="startMicrophone">
+        Enable Microphone
+        </button>
+
+        <p v-if="error">Error: {{ error.message }}</p>
+
+        <router-link to="/genres">
+        <button>Start Singing</button>
+        </router-link>
+    </div>
 </template>
+
+<script setup>
+    import { useMicrophone } from "../composables/useMicrophone.js"
+    const { startMicrophone, error } = useMicrophone()
+</script>
