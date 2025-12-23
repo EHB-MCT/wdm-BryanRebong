2ec21f63fd353365378f8c8bc62548077af9e870
BryanRebong <bryan.rebong@student.ehb.be>
2025-12-23 14:23:30 +0100

    used opencode to add more songs to my songs.js file

--- karaoke-frontend/src/data/songs.js
+++ karaoke-frontend/src/data/songs.js
@@ -2,21 +2,29 @@ export const songs = {
     pop: [
         { title: "Love Yourself", artist: "Justin Bieber" },
         { title: "Die For You", artist: "The Weeknd" },
-        { title: "Just The Way U Are", artist: "Bruno Mars" }
+        { title: "Just The Way U Are", artist: "Bruno Mars" },
+        { title: "Blinding Lights", artist: "The Weeknd" },
+        { title: "Levitating", artist: "Dua Lipa" }
     ],
     rock: [
         { title: "Don't Stop Believin'", artist: "Journey" },
         { title: "Thunderstruck", artist: "ACDC" },
-        { title: "Life Is a Highway", artist: "Rascall Flatts" }
+        { title: "Life Is a Highway", artist: "Rascall Flatts" },
+        { title: "Sweet Child O' Mine", artist: "Guns N' Roses" },
+        { title: "Livin' On A Prayer", artist: "Bon Jovi" }
     ],
     hiphop: [
         { title: "Beat It", artist: "Sean Kingston" },
         { title: "Wall To Wall", artist: "Chris Brown" },
-        { title: "Folded", artist: "Kehlani" }
+        { title: "Folded", artist: "Kehlani" },
+        { title: "Sicko Mode", artist: "Travis Scott" },
+        { title: "God's Plan", artist: "Drake" }
     ],
     kpop: [
         { title: "Fashion", artist: "CORTIS", audio: "/songs/fashion-cortis.mp3" },
         { title: "Crazy", artist: "LESSERAFIM" },
-        { title: "Jellyous", artist: "ILLIT" }
+        { title: "Jellyous", artist: "ILLIT" },
+        { title: "Dynamite", artist: "BTS" },
+        { title: "Kill This Love", artist: "BLACKPINK" }
     ]    
 };
