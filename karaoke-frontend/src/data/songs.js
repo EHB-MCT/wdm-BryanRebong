f372a6e8d7897a12d42c432457b1799001f1ddd6
BryanRebong <bryan.rebong@student.ehb.be>
2026-01-01 14:50:37 +0100

    added all the songs to the local database

--- karaoke-frontend/src/data/songs.js
+++ karaoke-frontend/src/data/songs.js
@@ -1,41 +1,106 @@
 export const songs = {
     pop: [
-        { title: "Love Yourself", artist: "Justin Bieber" },
-        { title: "Die For You", artist: "The Weeknd" },
-        { title: "Just The Way U Are", artist: "Bruno Mars" },
-        { title: "Blinding Lights", artist: "The Weeknd" },
-        { title: "Levitating", artist: "Dua Lipa" }
+        {
+            title: "What Do You Mean?",
+            artist: "Justin Bieber",
+            audio: "/songs/whatdoyoumean-justinbieber.mp3",
+            video: "/mv/whatdoyoumean-justinbieber.webm"
+        },
+        {
+            title: "thank u, next",
+            artist: "Ariana Grande",
+            audio: "/songs/thankunext-arianagrande.mp3",
+            video: "/mv/thankunext-arianagrande.webm"
+        },
+        {
+            title: "Sorry",
+            artist: "Justin Bieber",
+            audio: "/songs/sorry-justinbieber.mp3",
+            video: "/mv/sorry-justinbieber.webm"
+        },
+        {
+            title: "Sunflower",
+            artist: "Post Malone",
+            audio: "/songs/sunflower-postmalone.mp3",
+            video: "/mv/sunflower-postmalone.webm"
+        }
     ],
-    rock: [
-        { title: "Don't Stop Believin'", artist: "Journey" },
-        { title: "Thunderstruck", artist: "ACDC" },
-        { title: "Life Is a Highway", artist: "Rascall Flatts" },
-        { title: "Sweet Child O' Mine", artist: "Guns N' Roses" },
-        { title: "Livin' On A Prayer", artist: "Bon Jovi" }
+    rnb: [
+        {
+            title: "Is It A Crime",
+            artist: "NoGuidnce",
+            audio: "/songs/isitacrime-noguidnce.mp3",
+            video: "/mv/isitacrime-noguidnce.webm"
+        },
+        {
+            title: "Nokia",
+            artist: "PartyNextDoor",
+            audio: "/songs/nokia-partynextdoor.mp3",
+            video: "/mv/nokia-partynextdoor.webm"
+        },
+        {
+            title: "Something New",
+            artist: "Zendaya",
+            audio: "/songs/somethingnew-zendaya.mp3",
+            video: "/mv/somethingnew-zendaya.webm"
+        },
+        {
+            title: "Next Door",
+            artist: "Amilia Moore",
+            audio: "/songs/nextdoor-amiliamoore.mp3",
+            video: "/mv/nextdoor-amiliamoore.webm"
+        }
     ],
     hiphop: [
-        { title: "Beat It", artist: "Sean Kingston" },
-        { title: "Wall To Wall", artist: "Chris Brown" },
-        { title: "Folded", artist: "Kehlani" },
-        { title: "Sicko Mode", artist: "Travis Scott" },
-        { title: "God's Plan", artist: "Drake" }
+        {
+            title: "It Depends",
+            artist: "Chris Brown",
+            audio: "/songs/itdepends-chrisbrown.mp3",
+            video: "/mv/itdepends-chrisbrown.webm"
+        },
+        {
+            title: "Do You Mind",
+            artist: "Vedo",
+            audio: "/songs/doyoumind-vedo.mp3",
+            video: "/mv/doyoumind-vedo.webm"
+        },
+        {
+            title: "Die For You",
+            artist: "The Weeknd",
+            audio: "/songs/dieforyou-theweeknd.mp3",
+            video: "/mv/dieforyou-theweeknd.webm"
+        },
+        {
+            title: "Marry You",
+            artist: "Bruno Mars",
+            audio: "/songs/marryyou-brunomars.mp3",
+            video: "/mv/marryyou-brunomars.webm"
+        }
     ],
     kpop: [
-        { 
-            title: "Fashion", 
-            artist: "CORTIS", 
-            audio: "/songs/fashion-cortis.mp3",
-            video: "/mv/fashion-cortis.webm"
+        {
+            title: "My Universe",
+            artist: "Coldplay",
+            audio: "/songs/myuniverse-coldplay.mp3",
+            video: "/mv/myuniverse-coldplay.webm"
         },
-        { title: "Crazy", artist: "LESSERAFIM" },
-        { 
-            title: "Magnetic", 
-            artist: "ILLIT", 
-            audio: "/songs/magnetic-illit.mp3",
-            video: "/mv/magnetic-illit.webm"
+        {
+            title: "Touch",
+            artist: "KATSEYE",
+            audio: "/songs/touch-katseye.mp3",
+            video: "/mv/touch-katseye.webm"
         },
-        { title: "Jellyous", artist: "ILLIT" },
-        { title: "Dynamite", artist: "BTS" },
-        { title: "Kill This Love", artist: "BLACKPINK" }
+        {
+            title: "Extra",
+            artist: "Jennie",
+            audio: "/songs/extral-jennie.mp3",
+            video: "/mv/extral-jennie.webm"
+        },
+        {
+            title: "Free",
+            artist: "Kpop Demon Hunters",
+            audio: "/songs/free-kpopdemonhunters.mp3",
+            video: "/mv/free-kpopdemonhunters.webm"
+        }
     ]    
 };
